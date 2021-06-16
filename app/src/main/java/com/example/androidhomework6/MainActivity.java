a6dbe682c6474519b676fd8ae89233dd88003e90
V radiulov <radiulovv.a@gmail.com>
2021-06-16 22:00:13 +1000

    create menu

--- app/src/main/java/com/example/androidhomework6/MainActivity.java
+++ app/src/main/java/com/example/androidhomework6/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.androidhomework6;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        switch (id){
+            case R.id.settings:
+
+                return true;
+            case R.id.list:
+
+                return true;
+            case R.id.about:
+
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
